c705ff1d0b9dc65d9aeca03763a1738f418c53fa
2Bmodular <fabianodsbasilides@gmail.com>
2025-10-23 21:56:32 -0300

    frases aleatórias e cores nos ícones quando clicamos neles

--- app/src/main/java/com/example/appmotivacional/MainActivity.kt
+++ app/src/main/java/com/example/appmotivacional/MainActivity.kt
@@ -1,13 +1,18 @@
 package com.example.appmotivacional
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        val FraseRepository = FraseRepository()
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
@@ -15,6 +20,41 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+       var nome = findViewById<TextView>(R.id.text_saldacao)
+       var nomeRecupeacao =  this.getSharedPreferences("motivacao",MODE_PRIVATE).getString("nome","Olá Kotlin")
+   nome.text =nomeRecupeacao
+        var novaFrase = findViewById<TextView>(R.id.text_frase)
+        var botaoFrase = findViewById<Button>(R.id.button)
+        var imagemall = findViewById<ImageView>(R.id.image_infinito)
+        var imagemHappy = findViewById<ImageView>(R.id.image_carinha)
+        var imagemsun = findViewById<ImageView>(R.id.image_sol)
+
+        imagemsun.setOnClickListener {
+            imagemall.setColorFilter (ContextCompat.getColor(this,R.color.black))
+            imagemsun.setColorFilter (ContextCompat.getColor(this,R.color.white))
+            imagemHappy.setColorFilter (ContextCompat.getColor(this,R.color.black))
+            var fraseSorteada = FraseRepository.getFrase(2)
+            novaFrase.text = fraseSorteada
+        }
+        imagemall.setOnClickListener {
+            imagemall.setColorFilter (ContextCompat.getColor(this,R.color.white))
+            imagemsun.setColorFilter (ContextCompat.getColor(this,R.color.black))
+            imagemHappy.setColorFilter (ContextCompat.getColor(this,R.color.black))
+            var fraseSorteada = FraseRepository.getFrase(0)
+            novaFrase.text = fraseSorteada
+        }
+        imagemHappy.setOnClickListener {
+            imagemall.setColorFilter (ContextCompat.getColor(this,R.color.black))
+            imagemsun.setColorFilter (ContextCompat.getColor(this,R.color.black))
+            imagemHappy.setColorFilter (ContextCompat.getColor(this,R.color.white))
+            var fraseSorteada = FraseRepository.getFrase(1)
+            novaFrase.text = fraseSorteada
+        }
+
+        botaoFrase.setOnClickListener {
+            var fraseSorteada = FraseRepository.getFrase(0)
+            novaFrase.text = fraseSorteada
         }
     }
 }
--- app/src/main/java/com/example/appmotivacional/UserActivity.kt
+++ app/src/main/java/com/example/appmotivacional/UserActivity.kt
@@ -1,6 +1,10 @@
 package com.example.appmotivacional
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +19,18 @@ class UserActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        var buttonSalvar = findViewById<Button>(R.id.btnSalvar)
+        buttonSalvar.setOnClickListener {
+            var nomeText = findViewById<EditText>(R.id.nome).text
+            if (nomeText.isEmpty()) {
+                Toast.makeText(this, "Digite seu nome", Toast.LENGTH_LONG).show()
+            } else {
+                this.getSharedPreferences("motivacao",MODE_PRIVATE).edit().putString("nome",nomeText.toString()).apply()
+                var intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+            }
         }
     }
 }
